f659055e14f9b05ee3a942509442d01cdb970434
Jose Nunes <pedrocosta24@live.com.pt>
2018-10-14 18:33:44 +0100

    ID Exercise completed

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,72 @@ package com.example.android.magicidapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText etID;
+    Button btnSubmit;
+    TextView tvResults;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        etID=findViewById(R.id.etID);
+        btnSubmit=findViewById(R.id.btnSubmit);
+        tvResults=findViewById(R.id.tvResults);
+        tvResults.setVisibility(View.GONE);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                String idNumber=etID.getText().toString().trim();
+
+                String dateOfBirth=idNumber.substring(0,6);
+
+                Integer gender=Integer.parseInt(Character.toString(idNumber.charAt(6)));
+
+                String sGender;
+
+                if (gender<5)
+
+                    sGender=getString(R.string.female);
+                else
+                    sGender=getString(R.string.Male);
+
+                Integer nationality=Integer.parseInt(Character.toString(idNumber.charAt(10)));
+
+                String sNationality;
+
+                if (nationality==0)
+                    sNationality=getString(R.string.southAfrican);
+                else
+                    sNationality=getString(R.string.permanentResident);
+
+                String text = getString(R.string.dateOfBirth) + dateOfBirth + "\n" +
+                        getString(R.string.gender) + sGender + "\n" +
+                        getString(R.string.natiotanily) + sNationality;
+
+                tvResults.setText(text);
+                tvResults.setVisibility(View.VISIBLE);
+            }
+
+
+        });
+
     }
+
+
+
+
+
 }
